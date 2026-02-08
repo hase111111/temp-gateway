f381a67a8ec304fec3bb3c483a178b8ab23d6a11
hase111111 <hasehasehase61@gmail.com>
2026-02-08 19:05:39 +0900

    update: CANのシェルを追加

--- bash/can_startup.sh
+++ bash/can_startup.sh
@@ -6,18 +6,14 @@ TXQLEN=1000
 
 echo "Starting CAN interface: ${CAN_IF}"
 
-# 既に up していたら down
 if ip link show ${CAN_IF} > /dev/null 2>&1; then
     sudo ip link set ${CAN_IF} down
 fi
 
-# bitrate 設定
 sudo ip link set ${CAN_IF} type can bitrate ${BITRATE}
 
-# txqueuelen 設定
 sudo ip link set ${CAN_IF} txqueuelen ${TXQLEN}
 
-# up
 sudo ip link set ${CAN_IF} up
 
 echo "CAN interface status:"
