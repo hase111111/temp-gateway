919e8ce6630bba4162df1ba9eaedd74eabab3576
hase111111 <hasehasehase61@gmail.com>
2026-02-09 02:15:26 +0900

    a

--- pot_handler.cpp
+++ pot_handler.cpp
@@ -182,7 +182,7 @@ static void pot_loop() {
         }
 
         if (!has_request) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
             continue;
         }
 
